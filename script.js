a41e9be5f3eb801ae440a67c23d74a01219d128e
ayushbhandari356 <bhandaria351@gmail.com>
2024-01-25 04:04:28 +0530

    shop page

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Script for navigation bar
+console.log("starting js")
+let bar=document.getElementById("hamburgur");
+let nav=document.getElementById("navbar")
+let close=document.getElementById("ham-close")
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add("active");
+        console.log("ayush")
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove("active")
+    })
+}
